6facbddbfc0f1f919d3dc7620f256f75a1f8a4ca
Dylansweaver <dylanweaver@Dylans-Mac.local>
2017-02-05 22:26:48 -0600

    ButtonFunIOS290

---
+++ Button Fun/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Button Fun
+//
+//  Created by Dylan Sebastian Weaver on 1/30/17.
+//  Copyright © 2017 Dylan Sebastian Weaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let title = sender.title(for: .selected)!
+        let text = "\(title) button pressed"
+        let styledText = NSMutableAttributedString(string: text)
+        let attributes = [
+            NSFontAttributeName:
+                UIFont.boldSystemFont(ofSize: statusLabel.font.pointSize)
+        ]
+        let nameRange = (text as NSString).range(of: title)
+        styledText.setAttributes(attributes, range: nameRange)
+        statusLabel.attributedText = styledText
+        
+    }
+}
+
